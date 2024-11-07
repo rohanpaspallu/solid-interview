4ea55ee9988cd3557b09aec712c8a8206f23d8c9
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2024-11-07 12:21:54 -0500

    Added the
    Basic functionality implemented

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 
 export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState("#fff");
+  const randomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return `#${color}`;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        setBackgroundColor(randomColor);
+      }}
+    >
+      <View
+        style={[styles.container, { backgroundColor }]}
+        className={styles.container}
+      >
+        <Text>Hello There</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
